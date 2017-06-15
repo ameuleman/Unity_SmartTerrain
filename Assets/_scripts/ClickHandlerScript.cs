d6b59a3269ef3ade01bf3a7e0590532673e22948
ameuleman <andreasmeu@gmail.com>
2017-06-15 15:57:08 +0200

    Init

---
+++ Assets/_scripts/ClickHandlerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickHandlerScript : MonoBehaviour {
+    public SpawningScript spawningScript;
+
+    private const float CLICK_RESET_DELAY = 0.2f;
+    private float _lastClickDelta = 0;
+    private int _clickCount = 0;
+	
+	// Update is called once per frame
+	void Update () {
+        _lastClickDelta += Time.deltaTime;
+        if(_lastClickDelta > CLICK_RESET_DELAY)
+        {
+            _clickCount = 0;
+            _lastClickDelta = 0;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            _clickCount++;
+            if(_clickCount == 1)
+            {
+                if (gameObject)
+                {
+                    spawningScript.spawnCube();
+                }
+            }
+        }
+	}
+}
---
+++ Assets/_scripts/SpawningScript.cs
@@ -0,0 +1,27 @@
+﻿using Vuforia;
+using UnityEngine;
+
+public class SpawningScript : MonoBehaviour {
+    public Transform Cube;  
+
+    public void spawnCube()
+    {
+        GameObject go = VuforiaManager.Instance.ARCameraTransform.gameObject;
+        Camera[] cam = go.GetComponentsInChildren<Camera>();
+        Ray ray = cam[0].ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+
+
+
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            Vector3 spawnPoint = new Vector3(hitInfo.point.x, Cube.position.y, hitInfo.point.z);
+            int colliderCount = Physics.OverlapBox(spawnPoint, new Vector3(2, 2, 2), Quaternion.identity).Length;
+
+            if (colliderCount <= 1)
+            {
+                Transform cubeClone = (Transform)Instantiate(Cube, spawnPoint, Quaternion.identity);
+            }
+        }
+    }
+}
